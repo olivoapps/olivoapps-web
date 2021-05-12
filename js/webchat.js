287251bd417e4b32bcb8536172d4282d920b3383
Olivoapps <57659596+olivoapps@users.noreply.github.com>
2021-05-12 10:14:32 +0200

    Add files via upload

---
+++ js/webchat.js
@@ -0,0 +1,16 @@
+// Chatra
+/*
+window.ChatraSetup = {
+    colors: {
+        clientBubbleBg: '#eeeeee',
+		agentBubbleBg: '#c6eceb',
+		buttonText: '#eeeeee',
+		buttonBg: '#7f8a34'
+    	},
+	buttonPosition: 'br',
+	buttonSize: 40,
+	startHidden: true
+};
+
+(function(d, w, c) { w.ChatraID = 'xRHz5CouupK52vXYE'; var s = d.createElement('script'); w[c] = w[c] || function() { (w[c].q = w[c].q || []).push(arguments); }; s.async = true; s.src = 'https://call.chatra.io/chatra.js'; if (d.head) d.head.appendChild(s); })(document, window, 'Chatra');
+*/
